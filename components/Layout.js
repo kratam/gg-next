987c43563a3b351e6dc0a332d418acebdcfc1644
Tamás Krasser <krasser.tamas@gmail.com>
2021-01-13 18:14:58 +0100

    fix

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,12 @@
 import styles from '../styles/Home.module.css'
 import Header from './Header'
 
-export default function Layout({ children, showHeader = true }) {
+export default function Layout({ children, showHeader = true, footer = null }) {
   return (
     <div className={styles.container}>
       {showHeader && <Header />}
       <main className={styles.main}>{children}</main>
+      {footer}
     </div>
   )
 }
